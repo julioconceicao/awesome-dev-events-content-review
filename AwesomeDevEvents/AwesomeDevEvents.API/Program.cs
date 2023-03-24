40a696362770ab49d3d395201d60f82e90868300
Julio Cesar Goncalves Conceicao <julioconceicao.gtr@gmail.com>
2023-03-24 16:58:59 -0300

    add .vs files

--- AwesomeDevEvents/AwesomeDevEvents.API/Program.cs
+++ AwesomeDevEvents/AwesomeDevEvents.API/Program.cs
@@ -1,6 +1,9 @@
+using AwesomeDevEvents.API.Persistance;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddSingleton<DevEventsDbContext>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
